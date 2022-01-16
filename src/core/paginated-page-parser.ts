e7eaf4e60744b357112febb60987e2e3cb41f4c8
Tancred423 <thonkred@gmail.com>
2022-01-16 22:55:36 +0100

    Fixed FC icons; Fixed case sensitive error on worldname

--- src/core/paginated-page-parser.ts
+++ src/core/paginated-page-parser.ts
@@ -7,7 +7,7 @@ export abstract class PaginatedPageParser extends PageParser {
   protected getURL(req: Request): string {
     let query = ''
     if (req.query && req.query.page) {
-      query = `?page=${req.query?.page}`
+      query = `?page=${req.query.page}`
     }
     return `${this.getBaseURL(req)}${query}`
   }
--- src/search/character-search.ts
+++ src/search/character-search.ts
@@ -1,20 +1,29 @@
-import {PaginatedPageParser} from "../core/paginated-page-parser";
-import {CssSelectorRegistry} from "../core/css-selector-registry";
+import { PaginatedPageParser } from '../core/paginated-page-parser'
+import { CssSelectorRegistry } from '../core/css-selector-registry'
 import * as characterSearch from '../lib/lodestone-css-selectors/search/character.json'
-import {Request} from "express";
+import { Request } from 'express'
 
 export class CharacterSearch extends PaginatedPageParser {
-    protected getBaseURL(req: Request): string {
-        let query = `?q=${req.query.name}`;
-        if (req.query.dc) {
-            query += `&worldname=_dc_${req.query.dc}`;
-        } else if (req.query.server) {
-            query += `&worldname=${req.query.server}`;
-        }
-        return `https://na.finalfantasyxiv.com/lodestone/character/${query}`;
+  protected getBaseURL(req: Request): string {
+    let query = `?q=${req.query.name?.toString()?.replace(' ', '+')}`
+    if (req.query.dc) {
+      query += `&worldname=_dc_${req.query.dc}`
+    } else if (req.query.worldname) {
+      query += `&worldname=${this.formatWorldname(
+        req.query.worldname.toString()
+      )}`
     }
+    return `https://na.finalfantasyxiv.com/lodestone/character/${query}`
+  }
 
-    protected getCSSSelectors(): CssSelectorRegistry {
-        return characterSearch;
-    }
+  protected getCSSSelectors(): CssSelectorRegistry {
+    return characterSearch
+  }
+
+  private formatWorldname(worldname: string) {
+    return (
+      worldname.substring(0, 1).toUpperCase() +
+      worldname.substring(1).toLowerCase()
+    )
+  }
 }
