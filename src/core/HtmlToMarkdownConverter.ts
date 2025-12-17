12e197706d649db319b5190789748de7e9acefb6
yep.tancred <yep.tancred@gmail.com>
2025-12-17 09:33:15 +0000

    improve other timezone detection

--- src/core/HtmlToMarkdownConverter.ts
+++ src/core/HtmlToMarkdownConverter.ts
@@ -160,7 +160,7 @@ export class HtmlToMarkdownConverter {
     );
 
     text = text.replace(
-      /^(?:On\s+)?(?:From\s+)?(?:(?:Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday),\s+)?(?:\d{1,2}\s+)?[A-Za-z]{3,9}\.?\s*\d{1,2},?\s+\d{4}(?:\s+(?:at(?:\s+around)?|from))?\s+\d{1,2}:\d{2}(?:\s*[ap]\.?m\.?)?(?:\s+(?:to|and)\s+(?:(?:Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday),\s+)?(?:(?:\d{1,2}\s+)?[A-Za-z]{3,9}\.?\s*\d{1,2},?\s+\d{4}\s+)?(?:at\s+(?:around\s+)?)?\d{1,2}:\d{2}(?:\s*[ap]\.?m\.?)?)?\s+\((?!GMT)[A-Z]{2,5}\)\s*$/gm,
+      /^(?:On\s+)?(?:From\s+)?(?:(?:Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday),\s+)?(?:\d{1,2}\s+)?[A-Za-z]{3,9}\.?\s*\d{1,2},?\s+\d{4}(?:\s+(?:at(?:\s+around)?|from))?\s+\d{1,2}:\d{2}(?:\s*[ap]\.?m\.?)?(?:\s+(?:to|and)\s+(?:(?:Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday),\s+)?(?:(?:\d{1,2}\s+)?[A-Za-z]{3,9}\.?\s*\d{1,2},?\s+\d{4}\s+)?(?:at\s+(?:around\s+)?)?\d{1,2}:\d{2}(?:\s*[ap]\.?m\.?)?)?\s+\((?!GMT)[A-Z]{2,5}\)[.,!]?\s*$/gm,
       "",
     );
 
